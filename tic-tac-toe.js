3fb7fd11375f5b384dcd43633707f6e8a7cc8f28
Valou <anonanonymous018@gmail.com>
2025-09-28 14:16:38 +0200

    finish the game logic, decide who won and show it on the html

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -3,6 +3,8 @@ const TicTacToe = {
   gameTurn: 0
 }
 
+const winnerParagraph = document.querySelector('#winner-error')
+
 const players = [];
 
 function createPlayer (name, sign) {
@@ -17,18 +19,23 @@ function createPlayer (name, sign) {
 createPlayer('Cross player', 'X');
 createPlayer('Circle player', 'O');
 
+let currentPlayer = '';
+let winner;
+
 function makeAMove(tile, player) {
   if (player === 'Cross player') {
     TicTacToe.board[tile] = 'X';
+    currentPlayer = 'X'
   } else if (player === 'Circle player') {
     TicTacToe.board[tile] = 'O';
+    currentPlayer = 'O'
   }
   console.log(TicTacToe.board)
 }
 
 function checkFlow (tile) {
   if(checkIfGameIsWon() ) {
-    console.log(`The winner is ${player}!`)
+    console.log(`The winner is ${winner}!`)
     return
   }
 
@@ -48,7 +55,10 @@ function checkFlow (tile) {
 
     makeAMove (tile, player);
     if(checkIfGameIsWon() ) {
-    console.log(`The winner is ${player}!`)
+      if(TicTacToe.board[tile] === 'X') {
+        winner = 'Cross player'
+      } else winner = 'Circle player'
+    winnerParagraph.innerText = `The winner is ${player}!`
     return
   }
   } else console.log('You already played there!')
@@ -83,25 +93,24 @@ function checkIfGameIsWon (){
   }
 }
 
-/* const checkFlow = (function () {
-  const checkWin = () => {
-  let hasAnyoneWon ;
-  let case1, case2, case3, case4, case5, case6, case7, case8, case9;
+function linkButtonsToTicTacToeArray () {
+  const allButtons = document.querySelectorAll('#tic-tac-toe-container button')
+  console.log(allButtons)
+  allButtons.forEach((button) => {
+    button.innerText = '\u2060';
+    button.addEventListener('click', (event) => {
+      let lastCharacter = event.target.id.substr(event.target.id.length -1) -1
+      console.log(TicTacToe.board[lastCharacter]);
+      checkFlow(lastCharacter);
+      button.innerText = currentPlayer;
+    })
+  })
+}
+ linkButtonsToTicTacToeArray();
 
-  if 
-    hasAnyoneWon = true
 
-  } else if (case1 === case5 && case1 === case9 && case1 != undefined) {
-    hasAnyoneWon = true
-  } else if (case3 === case5 && case3 === case7 && case3 != undefined) {
-    hasAnyoneWon = true
-  } else {
-    hasAnyoneWon = false
-  } 
-  return hasAnyoneWon
-  }
 
-  const decideFirstPlayer = () => {
+/*   const decideFirstPlayer = () => {
     const random = Math.random();
     let firstPlayer;
 
@@ -120,66 +129,3 @@ function checkIfGameIsWon (){
 
   return { checkWin, decideFirstPlayer }
 })() */
-
-// const createGame = (function(game) {
-//     const TicTacToe = {
-//       game,
-//       board: '[]'
-//     }
-//         console.log(TicTacToe)
-//     return { TicTacToe };
-// })()
-
-// function createUser (name) {
-//   const discordName = "@" + name;
-
-//   let reputation = 0;
-//   const getReputation = () => reputation;
-//   const giveReputation = () => reputation++;
-
-//   return { name, discordName, getReputation, giveReputation };
-// }
-
-// const josh = createUser("josh");
-// josh.giveReputation();
-// josh.giveReputation();
-
-// console.log({
-//   discordName: josh.discordName,
-//   reputation: josh.getReputation()
-// });
-
-
-/* const calculator = (function () {
-    const add = (a, b) => a + b;
-    const sub = (a, b) => a - b;
-    const mul = (a, b) => a * b;
-    const div = (a, b) => a / b;
-    return { add, sub, mul, div };
-  })();
-  
-  calculator.add(3,5); // 8
-  calculator.sub(6,2); // 4
-  calculator.mul(14,5534); // 77476
-
-
-  const Formatter = (function() {
-    let timesRun = 0;
-  
-    const log = (message) => console.log(`[${Date.now()}] Logger: ${message}`);
-    const setTimesRun = () => { 
-      log("Setting times run");
-      ++timesRun;
-    }
-  
-    const makeUppercase = (text) => {
-      log("Making uppercase");
-      setTimesRun();
-      return text.toUpperCase();
-    };
-  
-    return {
-      makeUppercase,
-      timesRun,
-    }
-  })(); */

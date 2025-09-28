f6a1345e187d31c3ca451860ef8680d5cc249ca1
Valou <anonanonymous018@gmail.com>
2025-09-28 11:38:46 +0200

    complete the game logic in the console. Add buttons to act as cases

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,18 +1,60 @@
 const TicTacToe = {
-  board: [undefined,undefined,undefined,undefined,undefined,undefined,undefined,undefined,undefined]
+  board: [undefined,undefined,undefined,undefined,undefined,undefined,undefined,undefined,undefined],
+  gameTurn: 0
 }
+
+const players = [];
 
 function createPlayer (name, sign) {
-  return { name, sign }
+  function addPlayerToPlayersObject() {
+    players.push({name,sign})
+  }
+  addPlayerToPlayersObject();
 
+  return { name, sign }
 }
 
-const player1 = createPlayer('crossPlayer', 'X');
-const player2 = createPlayer('circlePlayer', 'O');
+createPlayer('Cross player', 'X');
+createPlayer('Circle player', 'O');
 
-const checkFlow = (function () {
-  const checkWin = () => {
-  let hasAnyoneWon ;
+function makeAMove(tile, player) {
+  if (player === 'Cross player') {
+    TicTacToe.board[tile] = 'X';
+  } else if (player === 'Circle player') {
+    TicTacToe.board[tile] = 'O';
+  }
+  console.log(TicTacToe.board)
+}
+
+function checkFlow (tile) {
+  if(checkIfGameIsWon() ) {
+    console.log(`The winner is ${player}!`)
+    return
+  }
+
+  if (TicTacToe.board[tile] === undefined) {
+
+    let player;
+    if (TicTacToe.gameTurn >8 ) return 'game over';
+
+    if (TicTacToe.gameTurn % 2  === 0) {
+      player = players[0].name;
+      TicTacToe.gameTurn += 1;
+
+    } else if (TicTacToe.gameTurn % 2  === 1) {
+      player = players[1].name;
+      TicTacToe.gameTurn += 1;
+    }
+
+    makeAMove (tile, player);
+    if(checkIfGameIsWon() ) {
+    console.log(`The winner is ${player}!`)
+    return
+  }
+  } else console.log('You already played there!')
+}
+
+function checkIfGameIsWon (){
   let case1, case2, case3, case4, case5, case6, case7, case8, case9;
   case1 = TicTacToe.board[0];
   case2 = TicTacToe.board[1];
@@ -24,18 +66,31 @@ const checkFlow = (function () {
   case8 = TicTacToe.board[7];
   case9 = TicTacToe.board[8];
 
-  if (case1 === case2 && case1 === case3 && case1 != undefined) {
+  let isGameWon;
+
+  if (
+    (case1 === case2 && case1 === case3 && case1 != undefined) || 
+    (case4 === case5 && case4 === case6 && case4 != undefined) ||
+    (case7 === case8 && case7 === case9 && case7 != undefined) ||
+    (case1 === case4 && case1 === case7 && case1 != undefined) ||
+    (case2 === case5 && case2 === case8 && case2 != undefined) ||
+    (case3 === case6 && case3 === case9 && case3 != undefined) ||
+    (case1 === case5 && case1 === case9 && case1 != undefined) ||
+    (case3 === case5 && case3 === case7 && case3 != undefined)) {
+    isGameWon = true;
+    console.log('Stop the count!');
+    return (isGameWon)
+  }
+}
+
+/* const checkFlow = (function () {
+  const checkWin = () => {
+  let hasAnyoneWon ;
+  let case1, case2, case3, case4, case5, case6, case7, case8, case9;
+
+  if 
     hasAnyoneWon = true
-  } else if (case4 === case5 && case4 === case6 && case4 != undefined) {
-    hasAnyoneWon = true
-  } else if (case7 === case8 && case7 === case9 && case7 != undefined) {
-    hasAnyoneWon = true
-  } else if (case1 === case4 && case1 === case7 && case1 != undefined) {
-    hasAnyoneWon = true
-  } else if (case2 === case5 && case2 === case8 && case2 != undefined) {
-    hasAnyoneWon = true
-  } else if (case3 === case6 && case3 === case9 && case3 != undefined) {
-    hasAnyoneWon = true
+
   } else if (case1 === case5 && case1 === case9 && case1 != undefined) {
     hasAnyoneWon = true
   } else if (case3 === case5 && case3 === case7 && case3 != undefined) {
@@ -60,11 +115,11 @@ const checkFlow = (function () {
 
   const checkTurn = () => {
     let turnCount = 0
-    const 
+
   }
 
   return { checkWin, decideFirstPlayer }
-})()
+})() */
 
 // const createGame = (function(game) {
 //     const TicTacToe = {
@@ -95,7 +150,7 @@ const checkFlow = (function () {
 // });
 
 
-const calculator = (function () {
+/* const calculator = (function () {
     const add = (a, b) => a + b;
     const sub = (a, b) => a - b;
     const mul = (a, b) => a * b;
@@ -127,4 +182,4 @@ const calculator = (function () {
       makeUppercase,
       timesRun,
     }
-  })();
+  })(); */
